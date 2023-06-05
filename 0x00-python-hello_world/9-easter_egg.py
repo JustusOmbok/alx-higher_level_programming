57b4f77eb735893174fc770783eac4d88585c189
JustusOmbok <ombokjustus@gmail.com>
2023-06-05 07:22:14 -0700

    prints zen of python

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-import The Zen of Python, by Tim Peters#!/usr/bin/python3
-import The Zen of Python, by Tim Peters#!/usr/bin/python3
-import The Zen of Python, by Tim Peters
+import this
