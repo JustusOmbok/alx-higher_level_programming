55aedb4a5a813410cf1c8f0e437196c38a544971
JustusOmbok <ombokjustus@gmail.com>
2023-06-08 03:09:53 -0700

    add function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-
 from add_0 import add
-if __nsme == "__main__":
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+if __name == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
