e432cd2f0c06e6bbd2f81da2628d414309698abf
JustusOmbok <ombokjustus@gmail.com>
2023-06-26 08:36:02 -0700

    prints an integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,8 +4,8 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(int(value)))
+        print("{:d}".format(value))
         return True
-    except (ValueError, TypeError):
+    except ValueError:
         print("Exception: Unknown format code 'd' for object of type '{}'".format(value.__class__.__name__), file=sys.stderr)
         return False
