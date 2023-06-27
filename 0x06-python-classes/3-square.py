e7ed3475c3b42a2a455b7c24d986e715037cd16c
JustusOmbok <ombokjustus@gmail.com>
2023-06-27 06:30:06 -0700

    empty class Square that defines a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a class Square that defines a square"""
+class Square:
+    """class defines square"""
+
+    def __int__(self, size=0):
+        """square instance is initialized"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """returns current square area"""
+        return self.__size ** 2
