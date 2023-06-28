79927cbcf281b53a4e5854232ed81c0b093ad990
JustusOmbok <ombokjustus@gmail.com>
2023-06-28 07:30:31 -0700

    singly linked list

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -23,6 +23,24 @@ class Square:
         else:
             self.__size = value
 
+    @property
+    def position(self):
+        """getter method to retrieve the position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """setter method to set the position"""
+        if(
+                not isinstance(value, tuple)
+                or len(value) != 2
+                or not all(isinstance(coord, int) forfor coord in value)
+                or any(coord < 0 for coord in value)
+            ):
+
+                raise TypeError("position must be a tuple")
+            self.__position = value
+
     def area(self):
         """returns the current square area"""
         return self.__size ** 2
@@ -32,5 +50,19 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for _ in range(self.__size):
-                print("#" * self.__size)
+            for _ in range(self.position[1]):
+                print()
+            for _ in range(self.size):
+                print(" " * self.position[0] + "#" * self.size)
+
+    def __str__(self):
+        """returns a string representation of the square"""
+        square_str = ""
+        if self.size == 0:
+            return square_str
+        else:
+            for _ in range(self.position[1]):
+                square_str += "\n"
+            for _ in range(self.size):
+                square_str += " " * self.position[0] + "#" * self.size + "\n"
+            return square_str.rstrip()
