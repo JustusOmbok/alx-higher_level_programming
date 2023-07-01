80a3d0fb0e7d6699a3072feba8a3b3363124b8c8
JustusOmbok <ombokjustus@gmail.com>
2023-07-01 19:16:25 +0300

    adds integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """
+    Adds two integers
+
+    Args:
+        a (int): First integer
+        b (int): Second integer (default is 98)
+
+    Returns:
+        int:    The addition of a nd b
+
+    Raises:
+        TypeError: If b or a is not ant integer or flaoat
+        
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
