58a5cbf4627567f6b5b6e9a5bec17be4d0bec2ff
JustusOmbok <ombokjustus@gmail.com>
2023-07-04 08:46:40 -0700

    getattr

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+"""prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name"""
 class LockedClass:
-    __slots__ = ["first_name"]
+    """locked class module"""
+    __slots__ = ['first_name']
