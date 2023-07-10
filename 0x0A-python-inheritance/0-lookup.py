ff46bee7f60f1843f0426a8fcfff05f074c875b2
JustusOmbok <ombokjustus@gmail.com>
2023-07-10 03:04:24 -0700

    list objects

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Returns list of attributes available and methods of an object"""
+
 def lookup(obj):
     """
     Returns list of attributes available and methods of an object
