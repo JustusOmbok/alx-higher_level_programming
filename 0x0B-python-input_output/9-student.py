8aad95da656f16241e410406b757437e5d98c62f
JustusOmbok <ombokjustus@gmail.com>
2023-07-12 03:55:28 -0700

    class student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,15 +7,11 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """a new instance is initialised"""
         self.first_name = first_name
-        self.lastname = last_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
         """
         a dict representation is retrieved
         """
-        return {
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'age': self.age
-        }
+        return vars(self)
