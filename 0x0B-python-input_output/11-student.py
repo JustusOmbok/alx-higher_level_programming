6dd3007a217e21ef3c948144c1706a8220266938
JustusOmbok <ombokjustus@gmail.com>
2023-07-12 06:09:59 -0700

    dict representation

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""a class representing a student
+"""
+class Student:
+    """
+    defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dictionary description"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            filtered_attrs = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    filtered_attrs[attr] = self.__dict__[attr]
+            return filtered_attrs
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
