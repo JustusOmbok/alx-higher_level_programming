703f729092af8c7fd34198310bbf24da1ff92b8a
JustusOmbok <ombokjustus@gmail.com>
2023-07-12 03:27:01 -0700

    dict description

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,10 +4,4 @@ def class_to_json(obj):
     """A dictionar, string, or integer description is returned
     """
     attributes = obj.__dict__
-    result = {}
-
-    for key in attributes:
-        if not callable(attributes[key]):
-            result[key] = attributes[key]
-
-    return result
+    return {key: attributes[key] for key in attributes if not callable(attributes[key])}
