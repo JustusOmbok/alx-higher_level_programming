13b7148ed214d952649c307f929b2a73192f3082
JustusOmbok <ombokjustus@gmail.com>
2023-07-15 05:59:50 -0700

    json representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Base module - base class is defined
 """
+import json
 
 class Base:
     """
@@ -23,3 +24,48 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        returns json string of a dictionary
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        json string representation of list_objs is written to file
+        """
+        filename = cls.__name__ + ".json"
+        json_list = []
+        if list_objs is not None:
+            json_list = [obj.to_dictionary() for obj in list_objs]
+        with open(filename, "w") as file:
+            file.write(cls.to_json_string(json_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        list represented by json string is returned
+        """
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        an instance with all attibutes is returned
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = cls()
+        dummy.update(**dictionary)
+        return dummy
