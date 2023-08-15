b2e159212ad90b024cdbd00d79b8d0cfa354a8c1
JustusOmbok <ombokjustus@gmail.com>
2023-08-15 15:51:46 +0300

    table tasks

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- Script adds new row
+INSERT first_table (id, name) VALUES (89, 'Best School');
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- Script displays number of records with id = 89
+SELECT COUNT(*) AS record_count FROM first_table WHERE id = 89;
