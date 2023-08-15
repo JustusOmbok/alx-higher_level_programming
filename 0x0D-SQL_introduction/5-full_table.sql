4b9b715c55d55ab11bbb26d7a7dfc3eb1611bec2
JustusOmbok <ombokjustus@gmail.com>
2023-08-15 14:55:16 +0300

    show table details

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Script that prints the full description of table
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY
-FROM COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
