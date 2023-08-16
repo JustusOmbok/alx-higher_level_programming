d81beda7ddd854558650ff720c1fd32da13a0f8f
JustusOmbok <ombokjustus@gmail.com>
2023-08-16 17:41:15 +0300

    creates user

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
 -- Script creates table force_name
 
 -- Creates tsble if not exists
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
