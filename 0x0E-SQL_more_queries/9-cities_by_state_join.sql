5b25b469a148ec76fc12fb06496518e0d0b7184a
JustusOmbok <ombokjustus@gmail.com>
2023-08-16 18:53:54 +0300

    joins tables

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,10 +1,7 @@
 -- Script lists all citie with their respective states
 
--- Using the database
-USE hbtn_0d_usa;
-
 -- List all cities with their respective states
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON cities.state_id = states.id
+INNER JOIN states ON cities.state_id=states.id
 ORDER BY cities.id;
