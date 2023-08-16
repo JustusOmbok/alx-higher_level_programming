b5f83691b8cef07f8f6d74420c8687825bd912a2
JustusOmbok <ombokjustus@gmail.com>
2023-08-16 18:41:27 +0300

    creates subquery

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,6 @@
 -- List cities in California
 -- Results sorted ascending by cities
 --Not allowed to use join
+
+USE hbtn_0d_usa;
 SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id;
