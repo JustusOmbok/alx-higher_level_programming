388c67b932f11a9883781e5a20c0f4e99bf61959
JustusOmbok <ombokjustus@gmail.com>
2023-08-16 18:37:58 +0300

    creates subquery

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,14 +1,5 @@
 -- Script lists cities of Califonia state
-
--- Using the database
-USE hbtn_0d_usa;
-
 -- List cities in California
-SELECT cities.id, cities.name
-FROM cities
-WHERE cities.state_id = (
-    SELECT id
-    FROM states
-    WHERE name = 'California'
-)
-ORDER BY cities.id;
+-- Results sorted ascending by cities
+--Not allowed to use join
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id;
