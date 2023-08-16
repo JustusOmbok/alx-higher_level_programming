1af8164d923eea5654e07184f91c8b1f81ae8f28
JustusOmbok <ombokjustus@gmail.com>
2023-08-16 17:06:43 +0300

    select privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,6 @@
 -- script to create database
 -- Apply changes
 -- Grants select permission
+
 -- Creates user
-
-
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2; 
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_od_2.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd' GRANT SELECT ON hbtn_od_2.* TO 'user_0d_2'@'localhost' FLUSH PRIVILEGES;
