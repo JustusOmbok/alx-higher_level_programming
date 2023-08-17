0e26e50357489ddfb3e68ad3bfb1483484c24403
JustusOmbok <ombokjustus@gmail.com>
2023-08-17 09:45:58 +0300

    show genres

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,8 @@
+-- Script that lists all shows not linked to genre Comedy
+-- Uses hbtn_0d_tvshows db
+-- Lists all ahows not having genre Comedy
+
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.id NOT IN (SELECT show_id FROM tv_show_genres WHERE genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy'))
+ORDER BY tv_shows.title;
