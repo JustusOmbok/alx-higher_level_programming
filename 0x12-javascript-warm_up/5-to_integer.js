0e0b9d929a9ff3220eda46b3e3a896dc3735d938
JustusOmbok <ombokjustus@gmail.com>
2023-09-11 18:34:18 +0300

    converting to integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const one = "Not a number';
+
+const args = process.argv[2];
+
+if (isNaN(parseInt(+args))) {
+	console.log(one);
+} else {
+	console.log('My number: ' + parseInt(+arg));
+}
