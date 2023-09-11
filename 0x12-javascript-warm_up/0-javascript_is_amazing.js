8186696adb30fabc02a5a9a08ccf4ac0d1aa2cd8
JustusOmbok <ombokjustus@gmail.com>
2023-09-11 14:07:46 +0300

    variable

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
 
-/*
-Creates a constant variable called myVar
-Uses console.log for output printing
-*/
-
-const myVar = "JavaScript is amazing";
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
