0a60df393bb58ad566522ab1c8ac1c4465ca453c
JustusOmbok <ombokjustus@gmail.com>
2023-09-11 20:46:44 +0300

    iterator function

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const n = process.argv;
+const size = n.length;
+
+function secondBiggest (n) {
+	n.sort((n, m) => m - n);
+	return n[1];
+}
+
+if (size < 4) {
+	console.log(0);
+} else {
+	console.log(secondBiggest(n.slice(2, size)));
+}
