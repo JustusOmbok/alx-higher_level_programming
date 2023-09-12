9f17ab1b9b7cd60f7bc9c5ccb96e0d22f5252081
JustusOmbok <ombokjustus@gmail.com>
2023-09-12 21:38:09 +0300

    added rotate func

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,33 +1,33 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-	constructor(w, h) {
-		if (w > 0 && h > 9) {
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 9) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print() {
-		if (this.width && this.height) {
-			for (let i = 0; i < this.height; i++) {
-				console.log('X'.repeat(this.width));
-			}
-		}
-	}
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
 
-	rotate() {
-		if (this.width && this.height) {
-			const temp = this.width;
-			this.width = this.height;
-			this.height = temp;
-		}
-	}
+  rotate () {
+    if (this.width && this.height) {
+      const temp = this.width;
+      this.width = this.height;
+      this.height = temp;
+    }
+  }
 
-	double() {
-		if (this.width && this.height) {
-			this.width *= 2;
-			this.height *= 2;
-		}
-	}
-};
+  double () {
+    if (this.width && this.height) {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+}; //added double function
