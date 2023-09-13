5da6e7369d9140b8b99599641d1f0f4c0f129a47
JustusOmbok <ombokjustus@gmail.com>
2023-09-13 09:40:49 +0300

    exports object

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,12 +3,12 @@
 const ParentSquare = require('./5-square');
 
 module.exports = class Square extends ParentSquare {
-	charPrint(c) {
-		if (c === undefined) {
-			c = 'X';
-		}
-		for (let i = 0; i < this.height; i++) {
-			console.log(c.repeat(this.width));
-		}
-	}
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 };
