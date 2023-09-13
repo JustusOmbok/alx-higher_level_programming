e5897fc844569c0377928115f6862dcfe5239463
JustusOmbok <ombokjustus@gmail.com>
2023-09-13 15:41:03 +0300

    file to destination

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const argv = process.argv;
+
+const source1 = fs.readFileSync(argv[2], 'utf8');
+const source2 = fs.readFileSync(argv[3], 'utf8');
+
+const des = source1 + source2;
+
+fs.writeFile(argv[4], des, function (err) {
+	if (err) {
+		return console.log(err);
+	}
+});
