5120240302cc398ea49badd1e4746651dfe4089d
JustusOmbok <ombokjustus@gmail.com>
2023-09-15 20:10:22 +0300

    against sql injection

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+ a script that lists all states 
+ from the database hbtn_0e_0_usa
+ """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    A script that lists all cities from the database hbtn_0e_4_usa
+
+Your script should take 3 arguments: mysql username, mysql password and database name
+    """
+    if len(argv) < 4:
+        exit()
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+
+    cur = db.cursor()
+
+    cur.execute("SELECT cities.id, cities.name, states.name, FROM cities \
+            JOIN states ON cities.state_id = states.id \
+            ORDER BY cities.id ASC")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
