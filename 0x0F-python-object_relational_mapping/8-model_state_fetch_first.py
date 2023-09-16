3c44a07051b6a32b96b7c7011b8d6c6eb1b5aa92
JustusOmbok <ombokjustus@gmail.com>
2023-09-16 13:10:28 +0300

    query firts state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,5 +18,10 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id):
-        print('{0}: {1}'.format(instance.id, instance.name))
+
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state is not None:
+        print('{}: {}'.format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
