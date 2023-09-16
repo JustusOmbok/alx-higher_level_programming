cee2fa66699b0e3b6d68ecccd88bdf2b7f3af3be
JustusOmbok <ombokjustus@gmail.com>
2023-09-16 09:18:48 +0300

    state cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
- A script that takes in the name of a state as an argument and lists all cities of that state, 
- using the database hbtn_0e_4_usa
+A script that takes in the name of a state as an argument and lists all cities of that state, 
+Using the database hbtn_0e_4_usa
  """
 import MySQLdb
 from sys import argv
@@ -9,10 +9,9 @@ from sys import argv
 if __name__ == "__main__":
     """
     A script that lists all cities from the database hbtn_0e_4_usa
-
-Your script should take 4 arguments: mysql username, mysql password and database name
+    Your script should take 4 arguments: mysql username, mysql password and database name
     """
-    if len(argv) < 4:
+    if len(argv) < 5:
         exit()
 
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
@@ -20,15 +19,18 @@ Your script should take 4 arguments: mysql username, mysql password and database
 
     cur = db.cursor()
 
-    query = "SELECT cities.id, cities.name FROM cities \
-            JOIN states ON cities.state_id = states.id \
-            WHERE states.name LIKE BINARY %(state_name)s \
-            ORDER BY cities.id ASC"
+    query = """
+    SELECT cities.id, cities.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name LIKE BINARY %s
+    ORDER BY cities.id ASC
+    """
 
-    cur.execute(query, {'state_name': argv[4]})
+    cur.execute(query, (state_name))
 
     rows = cur.fetchall()
 
     if rows is not None:
         for row in rows:
-            print(",".join([row[1]))
+            print([row[1])
