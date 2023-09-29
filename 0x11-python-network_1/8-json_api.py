0dc8097f2083a89c8d1bab2aea83a4d979c31977
JustusOmbok <ombokjustus@gmail.com>
2023-09-29 15:40:49 +0300

    fetches url

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Python script that takes in a letter and sends a POST request """
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+
+    payload = {'q': q}
+    url = 'http://0.0.0.0:5000/search_user'
+
+    try:
+        response = requests.post(url, data=payload)
+        data = response.json()
+
+        if data:
+            print("[{}] {}".format(data.get('id'), data.get('name')))
+        else:
+            print("No result")
+
+    except ValueError:
+        print("Not a valid JSON")
