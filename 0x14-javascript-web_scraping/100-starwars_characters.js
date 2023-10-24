bd8bea617993d1f4f90f2e6f538f6476d37797b0
JustusOmbok <ombokjustus@gmail.com>
2023-10-24 19:37:43 +0300

    star wars chars

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+
+request(apiUrl, (error, response, body) => {
+	if (error) {
+		console.error(error);
+	} else {
+		const movieData = JSON.parse(body);
+
+
+		for (const character of movieData.characters) {
+			request(character, (charError, charResponse, charBody) => {
+				if (charError) {
+					console.error(charError);
+				} else {
+					const charData = JSON.parse(charBody);
+					console.log(charData.name);
+				}
+			}
+		});
+	}
+});
