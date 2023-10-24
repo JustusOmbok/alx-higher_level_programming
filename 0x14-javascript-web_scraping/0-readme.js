3e22cb8297e8200d9566f00f410ae30fea6d7ab8
JustusOmbok <ombokjustus@gmail.com>
2023-10-24 12:51:09 +0300

    json read

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 //Reads the file in utf-8
-const fs = require('fs');
+const fs = require("fs");
 const filePath = process.argv[2];
 
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(filePath, "utf-8", (err, data) => {
 	if (err) {
 		console.error(err);
 	} else {
