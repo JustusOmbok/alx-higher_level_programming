2eeec360cf47f38d309994110233f7b1d27eb6d1
JustusOmbok <ombokjustus@gmail.com>
2023-10-24 15:11:54 +0300

    star wars api

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request')
+const apiUrl = process.argv[2];
+
+
+request(apiUrl, (error, response, body) => {
+	if (error) {
+		console.error(error);
+	} else {
+		if (response.statusCode === 200) {
+			const filmsData = JSON.parse(body);
+			const characterId= 18;
+			const moviesWithWedge = filmsData.results.filter((film) =>
+				film.characters.includes(`https://swapi-api.alx-tools.com/api/peaople/${characterId}/`)
+			);
+			console.log(movieData.title);
+		} else {
+			console.error(`Failed to retrieve movie data. Status code: ${response.statusCode}`);
+		}
+	}
+});
